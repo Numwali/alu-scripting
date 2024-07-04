a0668cb9b92d7ec97fcb73405e38855f9f5c05a4
Numwali <n.umwali@alustudent.com>
2024-07-04 07:48:57 +0000

    Task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Returning the number of subscribers for a given subreddit"""
+import json
 import requests
 
 
@@ -12,4 +13,5 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         return response.json().get('data') \
             .get('subscribers')
-    return 0
+    else: 
+     return 0
