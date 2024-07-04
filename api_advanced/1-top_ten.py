71576216bf24f56e616dde51487b27870ee3422e
Numwali <n.umwali@alustudent.com>
2024-07-04 08:45:12 +0000

    Task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-
-"""
-returns top 10 hot posts function
-"""
-
-import json
+"""" Top Ten Limit"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a subreddit."""
-    if len(sys.argv) < 2:
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
     else:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            print(None)
-        else:
-            data = json.loads(result.text)["data"]["children"]
-            for post in data[:10]:
-                print(post["data"]["title"])
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
