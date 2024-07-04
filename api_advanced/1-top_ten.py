2bd0bd616136b51ad5927ca17ca772992c8f3bba
Numwali <n.umwali@alustudent.com>
2024-07-04 09:18:04 +0000

    Task0

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-
 """
-get top 10 hot posts function
+1-top_ten.py: Top Ten
+
+This module contains a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit.
 """
 
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a subreddit."""
-    if len(sys.argv) < 2:
+    """Prints the top ten hot posts for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
         print(None)
-    else:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            print(None)
-        else:
-            data = json.loads(result.text)["data"]["children"]
-            for post in data[:10]:
-                print(post["data"]["title"])
+        return
+
+    data = response.json().get("data")
+    if data is None or len(data.get("children")) == 0:
+        print(None)
+        return
+
+    for child in data.get("children"):
+        print(child.get("data").get("title"))
