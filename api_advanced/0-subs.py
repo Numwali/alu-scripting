4ba42e2fac2a5a7662448c1aea1249eee8cd7684
Numwali <n.umwali@alustudent.com>
2024-07-04 08:13:07 +0000

    Task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""Returning the number of subscribers for a given subreddit"""
-import json
+"""
+Reddit API module to get the number of subscribers for a given subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returning the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    else: 
-     return 0
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid or an error occurs.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'reddit-subscriber-counter/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
