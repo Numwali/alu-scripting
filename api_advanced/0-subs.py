6dab53bd16ea3f11016d343e545437a4db0dfbce
Numwali <n.umwali@alustudent.com>
2024-07-04 09:03:09 +0000

    Task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""get subscribers numbers function"""
+"""
+A script that use reddit api to outputs the
+number of subscribers of a certain subreddit.
+"""
 
 
 import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """get all subscribers"""
-    if len(sys.argv) < 2:
-        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['data']['subscribers']
+
     else:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            return 0
-        body = json.loads(result.text)
-        return body["data"]["subscribers"]
+        return 0
