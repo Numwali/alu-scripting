15d0f442cc8e5fd4e792407b558acc70ae08aa31
Numwali <n.umwali@alustudent.com>
2024-07-04 09:05:55 +0000

    Task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-"""
-A script that use reddit api to outputs the
-number of subscribers of a certain subreddit.
-"""
+'''
+    This module contains the function number_of_subscribers
+'''
 
 
-import json
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-
+    '''
+        Returns the number of subscribers for a given subreddit
+    '''
+    user_agent = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data['data']['subscribers']
-
-    else:
+    try:
+        response = requests.get(url, headers=user_agent)
+        response.raise_for_status()  # Raise an error for bad status codes
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
         return 0
+    except KeyError:
+        print("Subreddit not found or unable to fetch data.")
+        return 0
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script_name.py subreddit_name")
+        exit(1)
+    subreddit_name = argv[1]
+    subscriber_count = number_of_subscribers(subreddit_name)
+    print(subscriber_count)
+
