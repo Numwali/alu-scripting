390fdf75fe393cb470a75e4004fbdf285a62e597
Numwali <n.umwali@alustudent.com>
2024-07-06 15:28:39 +0000

    Task0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""The script returns number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "LloydBot3.2.12 (by /akumuyi)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Return the number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
